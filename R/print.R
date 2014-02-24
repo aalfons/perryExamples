2de9749007d71d977ebbd680cfdb20424b6a40f2
Andreas Alfons <alfons@ese.eur.nl>
2014-02-25 00:11:08 +0100

    changes in print() methods

--- R/print.R
+++ R/print.R
@@ -4,43 +4,53 @@
 # --------------------------------------
 
 #' @S3method print ladlasso
-print.ladlasso <- function(x, ...) {
-#   # print function call
-#   if(!is.null(call <- x$call)) {
-#     cat("\nCall:\n")
-#     dput(call)
-#   }
+#' @S3method print lmridge
+print.ladlasso <- print.lmridge <- function(x, ...) {
   # print coefficients
   cat("\nCoefficients:\n")
   print(coef(x), ...)
-  # print penalty parameter
-  lambda <- as.matrix(x$lambda)
-  dimnames(lambda) <- list("Penalty parameter:", "")
-  print(lambda, ...)
   # return object invisibly
   invisible(x)
 }
 
-#' @S3method print lmridge
-print.lmridge <- function(x, ...) {
+# ## @S3method print ladlasso
+# print.ladlasso <- function(x, ...) {
 #   # print function call
 #   if(!is.null(call <- x$call)) {
 #     cat("\nCall:\n")
 #     dput(call)
 #   }
-  # print coefficients
-  cat("\nCoefficients:\n")
-  print(coef(x), ...)
-  # print penalty parameters
-  lambda <- x$lambda
-  if(length(lambda) == 1) {
-    lambda <- as.matrix(lambda)
-    dimnames(lambda) <- list("Penalty parameter:", "")
-    print(lambda, ...)
-  } else {
-    cat("\nPenalty parameters:\n")
-    print(lambda, ...)
-  }
-  # return object invisibly
-  invisible(x)
-}
+#   # print coefficients
+#   cat("\nCoefficients:\n")
+#   print(coef(x), ...)
+#   # print penalty parameter
+#   lambda <- as.matrix(x$lambda)
+#   dimnames(lambda) <- list("Penalty parameter:", "")
+#   print(lambda, ...)
+#   # return object invisibly
+#   invisible(x)
+# }
+#
+# ## @S3method print lmridge
+# print.lmridge <- function(x, ...) {
+#   # print function call
+#   if(!is.null(call <- x$call)) {
+#     cat("\nCall:\n")
+#     dput(call)
+#   }
+#   # print coefficients
+#   cat("\nCoefficients:\n")
+#   print(coef(x), ...)
+#   # print penalty parameters
+#   lambda <- x$lambda
+#   if(length(lambda) == 1) {
+#     lambda <- as.matrix(lambda)
+#     dimnames(lambda) <- list("Penalty parameter:", "")
+#     print(lambda, ...)
+#   } else {
+#     cat("\nPenalty parameters:\n")
+#     print(lambda, ...)
+#   }
+#   # return object invisibly
+#   invisible(x)
+# }
